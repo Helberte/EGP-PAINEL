850b40c5b33d17cbf74607cd652006893610fcfa
Helberte <helberteads@gmail.com>
2021-02-03 17:59:07 -0400

    Tela Login
    Tela Login

---
+++ EGP_Tela_Login/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace EGP_tela_login
+{
+    partial class Form_egp_login
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_egp_login));
+            this.ed_senha = new System.Windows.Forms.TextBox();
+            this.ed_usuario = new System.Windows.Forms.TextBox();
+            this.ed_codigo = new System.Windows.Forms.TextBox();
+            this.pictureBox_bt_sair = new System.Windows.Forms.PictureBox();
+            this.pictureBox_bt_acessar = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_bt_sair)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_bt_acessar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ed_senha
+            // 
+            this.ed_senha.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ed_senha.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ed_senha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.ed_senha.Location = new System.Drawing.Point(319, 336);
+            this.ed_senha.MaxLength = 8;
+            this.ed_senha.Multiline = true;
+            this.ed_senha.Name = "ed_senha";
+            this.ed_senha.Size = new System.Drawing.Size(201, 20);
+            this.ed_senha.TabIndex = 5;
+            this.ed_senha.Enter += new System.EventHandler(this.ed_senha_Enter);
+            this.ed_senha.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ed_senha_KeyPress);
+            this.ed_senha.Leave += new System.EventHandler(this.ed_senha_Leave);
+            // 
+            // ed_usuario
+            // 
+            this.ed_usuario.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ed_usuario.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ed_usuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ed_usuario.Location = new System.Drawing.Point(319, 277);
+            this.ed_usuario.MaxLength = 100;
+            this.ed_usuario.Multiline = true;
+            this.ed_usuario.Name = "ed_usuario";
+            this.ed_usuario.Size = new System.Drawing.Size(201, 20);
+            this.ed_usuario.TabIndex = 4;
+            this.ed_usuario.Enter += new System.EventHandler(this.ed_usuario_Enter);
+            this.ed_usuario.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ed_usuario_KeyPress);
+            this.ed_usuario.Leave += new System.EventHandler(this.ed_usuario_Leave);
+            // 
+            // ed_codigo
+            // 
+            this.ed_codigo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ed_codigo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.ed_codigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ed_codigo.Location = new System.Drawing.Point(319, 220);
+            this.ed_codigo.MaxLength = 100;
+            this.ed_codigo.Multiline = true;
+            this.ed_codigo.Name = "ed_codigo";
+            this.ed_codigo.Size = new System.Drawing.Size(201, 20);
+            this.ed_codigo.TabIndex = 3;
+            this.ed_codigo.Enter += new System.EventHandler(this.ed_codigo_Enter);
+            this.ed_codigo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ed_codigo_KeyPress);
+            this.ed_codigo.Leave += new System.EventHandler(this.ed_codigo_Leave);
+            // 
+            // pictureBox_bt_sair
+            // 
+            this.pictureBox_bt_sair.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox_bt_sair.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_bt_sair.Location = new System.Drawing.Point(423, 377);
+            this.pictureBox_bt_sair.Name = "pictureBox_bt_sair";
+            this.pictureBox_bt_sair.Size = new System.Drawing.Size(71, 36);
+            this.pictureBox_bt_sair.TabIndex = 6;
+            this.pictureBox_bt_sair.TabStop = false;
+            this.pictureBox_bt_sair.Click += new System.EventHandler(this.pictureBox_bt_sair_Click);
+            // 
+            // pictureBox_bt_acessar
+            // 
+            this.pictureBox_bt_acessar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox_bt_acessar.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_bt_acessar.Location = new System.Drawing.Point(346, 377);
+            this.pictureBox_bt_acessar.Name = "pictureBox_bt_acessar";
+            this.pictureBox_bt_acessar.Size = new System.Drawing.Size(71, 36);
+            this.pictureBox_bt_acessar.TabIndex = 7;
+            this.pictureBox_bt_acessar.TabStop = false;
+            this.pictureBox_bt_acessar.Click += new System.EventHandler(this.pictureBox_bt_acessar_Click);
+            // 
+            // Form_egp_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(838, 519);
+            this.Controls.Add(this.pictureBox_bt_acessar);
+            this.Controls.Add(this.pictureBox_bt_sair);
+            this.Controls.Add(this.ed_senha);
+            this.Controls.Add(this.ed_usuario);
+            this.Controls.Add(this.ed_codigo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form_egp_login";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_bt_sair)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_bt_acessar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ed_senha;
+        private System.Windows.Forms.TextBox ed_usuario;
+        private System.Windows.Forms.TextBox ed_codigo;
+        private System.Windows.Forms.PictureBox pictureBox_bt_sair;
+        private System.Windows.Forms.PictureBox pictureBox_bt_acessar;
+    }
+}
+
---
+++ EGP_Tela_Login/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EGP_tela_login
+{
+    public partial class Form_egp_login : System.Windows.Forms.Form
+    {
+        string texto_padrao_ed_codigo = "Digite seu código";
+        string texto_padrao_ed_usuario = "Digite seu nome ou e-mail";
+        string texto_padrao_ed_senha = "Digite sua senha de até 8 dígitos";
+
+        public Form_egp_login()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int eds_altura = 25;
+
+            ed_codigo.Height = eds_altura;
+            ed_codigo.Width = (this.Width / 3) - 130;
+            ed_codigo.Left = (this.Width / 2) - ((ed_codigo.Width / 2) + 2);
+            ed_codigo.Location = new Point(ed_codigo.Left, (this.Height / 2) - 56);
+            ed_codigo.Text = texto_padrao_ed_codigo;
+            ed_codigo.ForeColor = Color.FromArgb(160, 156, 153);
+
+            ed_usuario.Height = eds_altura;
+            ed_usuario.Width = ed_codigo.Width;
+            ed_usuario.Left = ed_codigo.Left;
+            ed_usuario.Location = new Point(ed_usuario.Left, ed_codigo.Location.Y + ed_codigo.Height + 63);
+            ed_usuario.Text = texto_padrao_ed_usuario;
+            ed_usuario.ForeColor = Color.FromArgb(160, 156, 153);
+
+            ed_senha.Height = eds_altura;
+            ed_senha.Width = ed_codigo.Width;
+            ed_senha.Left = ed_codigo.Left;
+            ed_senha.Location = new Point(ed_senha.Left, ed_usuario.Location.Y + ed_usuario.Height + 61);
+            ed_senha.Text = texto_padrao_ed_senha;
+            ed_senha.ForeColor = Color.FromArgb(160, 156, 153);
+
+            int bts_largura = 107;
+            int bts_altura = 42;
+
+            pictureBox_bt_acessar.Width = bts_largura;
+            pictureBox_bt_acessar.Left = ((this.Width / 2) - pictureBox_bt_acessar.Width) - 6;
+            pictureBox_bt_acessar.Height = bts_altura;
+            pictureBox_bt_acessar.Location = new Point(pictureBox_bt_acessar.Left, ed_senha.Location.Y + ed_senha.Height + 39);
+
+            pictureBox_bt_sair.Height = bts_altura;
+            pictureBox_bt_sair.Width = bts_largura;
+            pictureBox_bt_sair.Left = (this.Width / 2) + 9;
+            pictureBox_bt_sair.Location = new Point(pictureBox_bt_sair.Left, pictureBox_bt_acessar.Top);
+
+        }
+
+        private void ed_codigo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                if (e.KeyChar != (char)Keys.Back)
+                {
+                    if (e.KeyChar == (char) Keys.Enter)
+                    {
+                        e.Handled = true;
+                        ed_usuario.Focus();
+                    }
+                    else
+                    {
+                        e.Handled = true;
+                    }                   
+                }
+            }
+        }
+
+        private void ed_senha_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                ed_codigo.Focus();
+            }
+            else if (!char.IsDigit(e.KeyChar))
+            {
+                if (e.KeyChar != (char)Keys.Back)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void pictureBox_bt_sair_Click(object sender, EventArgs e)
+        {           
+            this.Close();
+        }
+
+        private void pictureBox_bt_acessar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Acessar");
+        }
+
+        private void ed_codigo_Enter(object sender, EventArgs e)
+        {
+            if (ed_codigo.Text == texto_padrao_ed_codigo)
+            {
+                ed_codigo.Clear();
+                ed_codigo.ForeColor = Color.Black;
+            }
+        }
+
+        private void ed_codigo_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ed_codigo.Text))
+            {
+                ed_codigo.Text = texto_padrao_ed_codigo;
+                ed_codigo.ForeColor = Color.FromArgb(160, 156, 153);
+            }
+        }
+
+        private void ed_senha_Enter(object sender, EventArgs e)
+        {
+            if (ed_senha.Text == texto_padrao_ed_senha)
+            {
+                ed_senha.Clear();
+                ed_senha.PasswordChar = '*';
+                ed_senha.ForeColor = Color.Black;
+            }
+        }
+
+        private void ed_senha_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ed_senha.Text))
+            {
+                ed_senha.ForeColor = Color.FromArgb(160, 156, 153);
+                ed_senha.PasswordChar = '\u0000';
+                ed_senha.Text = texto_padrao_ed_senha;
+            }           
+        }
+
+        private void ed_usuario_Enter(object sender, EventArgs e)
+        {
+            if (ed_usuario.Text == texto_padrao_ed_usuario)
+            {
+                ed_usuario.Clear();
+                ed_usuario.ForeColor = Color.Black;
+            }
+        }
+
+        private void ed_usuario_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(ed_usuario.Text))
+            {
+                ed_usuario.Text = texto_padrao_ed_usuario;
+                ed_usuario.ForeColor = Color.FromArgb(160, 156, 153);
+            }
+        }
+
+        private void ed_usuario_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar("'"))
+            {
+                e.Handled = true;
+            }
+            else if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                ed_senha.Focus();
+            }
+            else 
+            if (string.IsNullOrWhiteSpace(Convert.ToString(e.KeyChar)))
+            {
+                e.Handled = true;
+            }
+            else
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                ed_usuario.Focus();
+            }            
+        }
+    }
+}
